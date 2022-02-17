74f7507617d8d53c1dc0c8a333bfab9681b0b4e8
Hoa Vo <ngochoa2603@gmail.com>
2022-02-17 21:30:21 +0700

    update Topic_01_Check_Environment

--- src/webdriver/Topic_01_Check_Environment.java
+++ src/webdriver/Topic_01_Check_Environment.java
@@ -20,6 +20,7 @@ public class Topic_01_Check_Environment {
 		driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
+		
 		driver.get("https://www.facebook.com/");
 	}
 
